8e27961eeac42f8ffec327d633f4f35a4e0d910c
Kathlen Graziela <kathlengraziela@gmail.com>
2022-08-31 09:54:33 -0300

    1ª versão funcionando.

--- Escola/Aluno.cs
+++ Escola/Aluno.cs
@@ -1,10 +1,12 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
 
 namespace Escola
 {
